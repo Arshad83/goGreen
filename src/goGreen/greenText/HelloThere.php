62c5e743a2d6a93660a2e1e4861dd5f4429c7036
syedarshadali <arshad@3esofttech.com>
2018-04-20 15:23:49 +0530

    Basic composer learning

---
+++ src/goGreen/greenText/HelloThere.php
@@ -0,0 +1,14 @@
+<?php
+namespace goGreen\greenText;
+/**
+ * A terribly useful class
+ *
+ * See: http://knpuniversity.com/screencast/question-answer-day
+ */
+class HelloThere
+{
+    public function sayHallo()
+    {
+        return 'Hello!';
+    }
+}
